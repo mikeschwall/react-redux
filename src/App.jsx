c4e08f94569ca33cd86a06a8daa45332dff5f228
Mike Schwall <mikschwall@gmail.com>
2025-03-06 11:48:05 -0600

    adding todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import MainComponent from './MainComponent/MainComponent'
+import TodoList from './TodoList/TodoList'
+import axios from 'axios';
 
 function App() {
+
+  const [todos,setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then(response => setTodos(response.data));
+  },[]);
   
+
+  
+
   return (
-   <MainComponent/>
+    <div>
+      <TodoList/>
+      {todos && todos.filter(item => item.id < 11).map(item => <li key={item.id}>{item.title}</li>)}
+      <hr/>
+      <MainComponent/>
+    </div>
   )
 }
 
---
+++ src/TodoList/TodoList.jsx
@@ -0,0 +1,15 @@
+
+
+const TodoList = () => {
+
+    return (
+        <div>
+            <h2>Todos</h2>
+            <form>
+                <input type="text"/>
+            </form>
+        </div>
+    )
+}
+
+export default TodoList;
