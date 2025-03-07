460dc6faa6e65e9abcf1ff1cb8d9809f270e0349
Mike Schwall <mikschwall@gmail.com>
2025-03-07 15:02:59 -0600

    more housekeeping stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthContextProvider } from './Store/context'
-import { Provider } from 'react-redux'
-import combineReducers from './reducers';
-import { createStore } from 'redux'
-const store = createStore(combineReducers);
-console.log(store.getState());
+
+
 
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  
+    <App/>,
 )
