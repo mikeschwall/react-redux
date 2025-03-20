e4168852b1cf8b557bba3a8390971a6ca42435d0
Mike Schwall <mikschwall@gmail.com>
2025-03-20 09:23:17 -0500

    cleaning repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import SongList from './SongList'
-import TodoList from './TodoList'
 
 function App() {
 
   return (
     <div>
-      <SongList/>
-      <hr/>
-      <TodoList/>
+      test
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TodoList from './TodoList.jsx'
-import { Provider } from 'react-redux';
-import { store } from './store/index.js'
+
 
 
 
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}><App/></Provider>
+  <App/>
     ,
 )
